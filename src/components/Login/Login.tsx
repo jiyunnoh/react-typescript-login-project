44947a9953676a7577cf2cb010382041a2aa65af
jiyunnoh <ianjy1127@gmail.com>
2022-03-27 23:36:46 -0500

    114. Using the useEffect Cleanup Function

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -13,9 +13,17 @@ const Login = (props: { onLogin: (arg0: string, arg1: string) => void; }) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log('Checking the form validity.')
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('Clean Up!')
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,9 +51,8 @@ const Login = (props: { onLogin: (arg0: string, arg1: string) => void; }) => {
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
-          }`}
+          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+            }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
@@ -57,9 +64,8 @@ const Login = (props: { onLogin: (arg0: string, arg1: string) => void; }) => {
           />
         </div>
         <div
-          className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
-          }`}
+          className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''
+            }`}
         >
           <label htmlFor="password">Password</label>
           <input
