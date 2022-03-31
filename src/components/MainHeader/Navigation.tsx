4743f5c107b79eb61648510eb1a15595200e1235
jiyunnoh <ianjy1127@gmail.com>
2022-03-31 16:33:51 -0500

    123. useContext hook

--- src/components/MainHeader/Navigation.tsx
+++ src/components/MainHeader/Navigation.tsx
@@ -1,37 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props: { onLogout: React.MouseEventHandler<HTMLButtonElement> | undefined; }) => {
+  const ctx = useContext(AuthContext);
   return (
-    /* function child를 가진다. */
-    /* argument로 context data를 받아온다. 이 경우 isLoggedIn */
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
